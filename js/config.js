e0cdb1f4296a2fa01f6d98761bc87584cfa86b6e
acharism <achmad.charis.m@gmail.com>
2026-07-05 15:17:56 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+/* ==========================================
+   NewsCraft AI
+   File : config.js
+========================================== */
+
+const API_KEY = "AQ.Ab8RN6JMO3M9-hZW0EA6VHKZy_ai3FNpIlZLsjRJZVdhSTMW8g";
